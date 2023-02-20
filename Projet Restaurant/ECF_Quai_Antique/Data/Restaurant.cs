dd2bc28a005b7536e5c2534a8ed25b56da166d90
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 14:58:42 +0100

    Class Restaurant

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class Restaurant
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private int Guest { get; set; }
+
+    }
+}
