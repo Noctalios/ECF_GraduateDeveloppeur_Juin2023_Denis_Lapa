948653d6d134583ff546d17f1d14a7ac431c49e1
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 16:39:36 +0100

    Menu entity and gitignore

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Menu.cs
@@ -0,0 +1,20 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class Menu
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private string Description { get; set; }
+
+        private List<Formula> Formulas { get; set; }
+
+        public Menu(string name, string description, List<Formula> formulas) 
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Formulas = formulas;
+        }
+    }
+}
