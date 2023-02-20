9a9d644c38a438537c0a0e63a523aee981b02857
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 15:35:04 +0100

    Formula entity

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Formula.cs
@@ -0,0 +1,20 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class Formula
+    {
+        private int Id { get; set; }
+
+        private string Description { get; set; }
+
+        private decimal Price { get; set; }
+
+        private List<DishType> DishTypes { get; set; }
+
+        public Formula(string description, decimal price, List<DishType> DishTypes) 
+        {
+            this.Description = description;
+            this.Price = price;
+            this.DishTypes = DishTypes;
+        }
+    }
+}
