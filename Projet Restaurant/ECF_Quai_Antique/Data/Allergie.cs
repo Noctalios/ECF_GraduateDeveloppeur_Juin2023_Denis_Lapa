748bdd09972d5fbb49316a8813219dc951b33bbb
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 16:44:55 +0100

    Allergies entity

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Allergie.cs
@@ -0,0 +1,14 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class Allergie
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private Allergie(string name)
+        {
+            Name = name;
+        }
+    }
+}
