3c2be48a1cd132b108624714c5935a1eb183382a
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 17:19:52 +0100

    Booking Entity

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Booking.cs
@@ -0,0 +1,23 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class Booking
+    {
+        private int Id { get; set; }
+     
+        private DateTime Date { get; set; }
+        
+        private int ClientId { get; set; }
+        
+        private int Guest { get; set; }
+
+        private List<Allergie> Allergens { get; set; }
+
+        public Booking(DateTime date, int clientId, int guest, List<Allergie> allergens)
+        {
+            this.Date = date;
+            this.ClientId = clientId;
+            this.Guest = guest;
+            this.Allergens = allergens;
+        }
+    }
+}
