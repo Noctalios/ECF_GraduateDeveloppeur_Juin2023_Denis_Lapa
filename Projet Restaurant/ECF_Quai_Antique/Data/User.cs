065f93bf49c5bdf7ea731b8a8a24693fd44d6e72
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 14:34:37 +0100

    Class user et tentatives de git ignore

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ECF_Quai_Antique.Data;
+
+public class User
+{
+    private int Id { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public int Role { get; set; }
+
+    private int? Guest { get; set; }
+
+    public User(string email, string password, int guest) 
+    {
+        this.Email = email;
+        this.Password = password;
+        this.Guest = guest;
+    }
+}
--- Projet Restaurant/ECF_Quai_Antique/Data/WeatherForecastService.cs
+++ Projet Restaurant/ECF_Quai_Antique/Data/WeatherForecastService.cs
@@ -5,7 +5,7 @@ public class WeatherForecastService
     private static readonly string[] Summaries = new[]
     {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    };
 
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
