0f4947cc6db5d3d4b6f049e1f438505db4abb03f
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-20 17:50:30 +0100

    Work days entities

---
+++ Projet Restaurant/ECF_Quai_Antique/Data/Period.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ECF_Quai_Antique.Data
+{
+    public class Period
+    {
+        private int Id { get; set; }
+
+        private int Service { get; set; }
+
+        private decimal Open { get; set; }
+
+        private decimal Close { get; set; }
+
+        public Period(int id, int service, decimal open, decimal close)
+        {
+            Id = id;
+            Service = service;
+            Open = open;
+            Close = close;
+        }
+    }
+}
---
+++ Projet Restaurant/ECF_Quai_Antique/Data/WorkDay.cs
@@ -0,0 +1,15 @@
+﻿namespace ECF_Quai_Antique.Data
+{
+    public class WorkDay
+    {
+        private DayOfWeek DayOfWeek { get; set; }
+
+        private List<Period> periods { get; set; }
+
+        public WorkDay(DayOfWeek dayOfWeek, List<Period> periods)
+        {
+            this.DayOfWeek = dayOfWeek;
+            this.periods = periods;
+        }
+    }
+}
