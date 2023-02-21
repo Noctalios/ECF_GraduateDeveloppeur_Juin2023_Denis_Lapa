a01d506bd0285b2c25fe35694491415bf8820249
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-21 14:07:39 +0100

    Add of Dish Type table

--- Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Restaurant.sql
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Restaurant.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Restaurant] 
 (
     [Id]    INT          IDENTITY (1, 1),
-    [Name]  VARCHAR (50) NOT NULL,
+    [Label] VARCHAR (50) NOT NULL,
     [Guest] INT          NOT NULL,
     PRIMARY KEY (Id),
 );
