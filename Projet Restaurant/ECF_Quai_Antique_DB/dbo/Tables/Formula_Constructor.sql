8c61efff064cfe405d2a41b0173ebf20e0d5c88c
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-21 14:33:07 +0100

    Creation of table related to formulas that constitued menues

---
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Formula_Constructor.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Formula_Constructor]
+(
+	[Id]			INT IDENTITY (1, 1),
+	[FormulaId]		INT NOT NULL,
+	[DishTypeId]	INT NOT NULL,
+	PRIMARY KEY (Id),
+	CONSTRAINT FK_FC__Formula	FOREIGN KEY (FormulaId)		REFERENCES Formula(Id),
+	CONSTRAINT FK_FC__DishType	FOREIGN KEY (DishTypeId)	REFERENCES DishType(Id)
+);
