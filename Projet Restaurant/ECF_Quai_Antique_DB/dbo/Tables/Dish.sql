73ecffb920579f83c010991f4ff4964355751e24
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-21 14:16:58 +0100

    Add of Dish table

---
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Dish.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Dish]
+(
+	[Id]			INT				IDENTITY(1, 1),
+	[DishTypeId]	INT				NOT NULL,
+	[Description]	VARCHAR(MAX)	NOT NULL,
+	[Price]			DECIMAL(12, 2)	NOT NULL,
+	PRIMARY KEY (Id),
+	CONSTRAINT FK_Dishes__DishType FOREIGN KEY (DishTypeId) REFERENCES DishType(Id),
+);
