2a2f5626fc754b9f76c796a002dbe674906908eb
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-22 13:49:31 +0100

    Creation Of the proc stock to retrieve the User

---
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Stored Procedures/UserConnexion.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Lapa Denis
+-- Create date: 22/02/2022
+-- Description:	During the connexion retrieve the user
+-- =============================================
+CREATE PROCEDURE [dbo].[UserConnexion] 
+	-- Add the parameters for the stored procedure here
+	@Email VARCHAR(255)
+AS
+	BEGIN
+		SET NOCOUNT ON;
+		SELECT u.Id, U.Email, U.Password, U.Guest, U.RoleId, R.Label
+		FROM Users U
+		LEFT JOIN Roles R ON R.Id = U.RoleId
+		WHERE U.Email = @Email;
+	END
--- Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Users.sql
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Users.sql
@@ -3,7 +3,7 @@
 	[Id]		INT				IDENTITY (1, 1),
 	[Email]		VARCHAR(255)	NOT NULL,
 	[Password]	VARCHAR(MAX)	NOT NULL,
-	[Guest]		INT				NOT NULL,
+	[Guest]		INT				NULL,
 	[RoleId]	INT				NOT NULL,
 	PRIMARY KEY (Id),
 	CONSTRAINT FK_User__Role FOREIGN KEY (RoleId) REFERENCES Roles(Id)
