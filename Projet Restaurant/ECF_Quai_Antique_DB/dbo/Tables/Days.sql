b43c8f089d9522d3a53968f34dd218e5c86c355f
Denis Lapa <lapa.denis.pro@gmail.com>
2023-02-23 18:30:29 +0100

    ADD of Working days table related

---
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/Days.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Days]
+(
+	[Id]			INT IDENTITY (1, 1),
+	[Label]			VARCHAR(10) NOT NULL,
+	[RestaurantId]	INT			NOT NULL,
+	PRIMARY KEY (Id),
+	CONSTRAINT FK_Days__Restaurant FOREIGN KEY (RestaurantId) REFERENCES Restaurant(Id)
+)
---
+++ Projet Restaurant/ECF_Quai_Antique_DB/dbo/Tables/WorkingDays.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Periods]
+(
+	[Id]	INT NOT NULL,
+	[DayId] INT NOT NULL,
+	[Open]	DECIMAL(2, 2) NULL,
+	[Close]	DECIMAL(2, 2) NULL,
+    PRIMARY KEY (Id),
+	CONSTRAINT FK_Periods__Days FOREIGN KEY (DayId) REFERENCES Days(Id) 
+)
